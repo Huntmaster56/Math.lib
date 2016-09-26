122fc3175dfb96db5db25189f8bba577fea429db
joshua.livingst <joshua.livingst@SEA-8CC916>
2016-09-26 10:59:58 -0700

    initial configurations

---
+++ MathLib/Test.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+
+int doNothing(int x)
+{
+
+	return x*x + 2*x + 5;
+}
---
+++ MathTests/main.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <cstdio>
+
+#include "test.h"
+int main()
+{
+	printf("Hello World! %d", doNothing(40));
+
+
+
+	getchar();
+	return 0;
+
+
+
+}
