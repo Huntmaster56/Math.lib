6f8a88e6bc1af13974492504054a1ab51185340a
joshua.livingst <joshua.livingst@SEA-8CC916>
2016-09-28 12:21:52 -0700

    things happened

--- Components/Transform.cpp
+++ Components/Transform.cpp
@@ -1,5 +1,22 @@
 #include "Transform.h"
 #include "sfwdraw.h"
+
+using namespace sfw;
+Transform::Transform() : facing(0), position({ 0,0 }), scale({28,8})
+{
+		//position.x = 0;
+		//position.y = 0;
+		//scale.x = 28;
+		//scale.y = 8;
+}
+
+Transform::Transform(float x, float y, float w, float h, float a)
+		  : position{ x,y }, scale{ w,h }, facing{ a }
+{
+	//position.x = x;
+	//position.y = y;
+}
+
 vec2 Transform::getDirection()
 {
 	return fromAngle(facing);
@@ -12,15 +29,40 @@ void Transform::setDirection(const vec2 &dir)
 
 void Transform::debugDraw()
 {
-	sfw::drawCircle(position.x, position.y, 12);
+	sfw::drawCircle(position.x, position.y, 96);
 
-	vec2 dirEnd = position + getDirection() * scale.x * 4;
-	vec2 upEnd = position - perp(getDirection()) * scale.y * 4;
+	vec2 dirEnd = position + getDirection() * scale.x * 8;
+	vec2 upEnd = position - perp(getDirection()) * scale.y * 6;
 
 
 	sfw::drawLine(position.x,	position.y,
 				  dirEnd.x,		dirEnd.y, GREEN);
 
 	sfw::drawLine(position.x,	position.y,
-				  upEnd.x,			upEnd.y, BLUE);
+				  upEnd.x,		upEnd.y,  BLUE);
+
+	if (getKey(KEY_UP))
+	{
+		position -= getDirection() * 5;
+	}
+
+	else if (getKey(KEY_LEFT))
+	{
+		facing += .1;
+	}
+
+	else if (getKey(KEY_RIGHT))
+	{
+		facing -= .1;
+	}
+
+	else if (getKey(KEY_DOWN))
+	{
+		position += getDirection() * 5;
+	}
+
+
+
+
+
 }
--- Components/main.cpp
+++ Components/main.cpp
@@ -7,14 +7,34 @@ using namespace sfw;
 void main()
 {
 	sfw::initContext();
-	Transform trans;
+	Transform trans(400, 300, 30,30, 23);
 
-	trans.position = vec2{ 400,300 };
+	int j = int(4);
+	int k(4);
+	int l = { 4 };
+	int n{ 4 };
+
+	Transform tl = Transform(400, 300);
+	Transform tn = { 400, 300 };
+	Transform tm(400, 300);
+	Transform tp{ 400, 300 };
+
+
+
+//	trans.position = vec2{ 400,300 };
 	trans.facing = deg2rad(45);
-	trans.scale = vec2{ 12,8 };
+	trans.scale = vec2{ 24,16 };
+
+	vec2 basis = { 40,0 };
+	float ang_vec = 0;
 
 	while (sfw::stepContext())
 	{
+		ang_vec += sfw::getDeltaTime();
+		vec2 incident = fromAngle(ang_vec) * 40;
+		float proj = dot(basis, incident);
+		drawLine(400, 300, 400 + basis.x + basis.y, BLACK);
+
 		trans.debugDraw();
 		trans.facing += getDeltaTime();
 	}
